fc81e1e116d6e5edbba3c6a7c6a5fe4c030cadef
pankajthayat <pankajthayat@gmail.com>
2019-01-24 22:26:58 +0530

    module variable in node

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ console.log("starting app");
 const fs =require('fs');// require lets us load the module functionality and call is down below
 const os=require('os');
 var user=os.userInfo();
-console.log(user);
-fs.appendFile('greeting.text',`Hello ! ${user.username}`,(err)=>{
+
+const notes=require('./notes');// give reletive path... this is nota module but a file... so reletive path
+
+fs.appendFile('greeting.text',`Hello ! ${user.username}. you are ${notes.age}`,(err)=>{
     if(err)
     {
         console.log("unable to append the file");
--- notes.js
+++ notes.js
@@ -1,1 +1,7 @@
-console.log("starting notes.js")
+console.log("starting notes.js");
+
+
+console.log(module);// inside all node file we have access to this variable 'module'
+//export is an object in module property
+
+module.export.age=26;
