03c1e2e84ebec28d508720cde5efa251e5e939d9
pankajthayat <pankajthayat@gmail.com>
2019-01-24 22:52:24 +0530

    lodash module

--- app.js
+++ app.js
@@ -5,14 +5,22 @@ var user=os.userInfo();
 
 const notes=require('./notes');// give reletive path... this is nota module but a file... so reletive path
 
-fs.appendFile('greeting.text',`Hello ! ${user.username}. you are ${notes.age}`,(err)=>{
-    if(err)
-    {
-        console.log("unable to append the file");
-    }
-    else{
-        console.log("append successfully");
-    }
-})
 
-//most of Node's API will pass an error as the first argument to all of its functions known as error-first callbacks
+const _ =require('lodash');
+console.log(_.isString("pankaj"))
+console.log(_.isString(true))
+
+var filteredArray=_.uniq(["pankaj",1,"pankaj",1,2])
+console.log(filteredArray)
+
+// fs.appendFile('greeting.text',`Hello ! ${user.username}. you are ${notes.age}`,(err)=>{
+//     if(err)
+//     {
+//         console.log("unable to append the file");
+//     }
+//     else{
+//         console.log("append successfully");
+//     }
+// })
+
+// //most of Node's API will pass an error as the first argument to all of its functions known as error-first callbacks
