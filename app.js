d8d67fa14d2703bf16a6bb0fd0dd6db4de93d050
pankajthayat <pankajthayat@gmail.com>
2019-01-24 22:11:27 +0530

    require function

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+console.log("starting app");
+const fs =require('fs');
+fs.appendFile('greeting.text','Hello world !',(err)=>{
+    if(err)
+    {
+        console.log("unable to append the file");
+    }
+    else{
+        console.log("append successfully");
+    }
+})
