5037dc97b3c1cb3910d95559aea8e59ea052cd30
pankajthayat <pankajthayat@gmail.com>
2019-01-24 22:20:06 +0530

    os module

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 console.log("starting app");
-const fs =require('fs');
-fs.appendFile('greeting.text','Hello world !',(err)=>{
+const fs =require('fs');// require lets us load the module functionality and call is down below
+const os=require('os');
+var user=os.userInfo();
+console.log(user);
+fs.appendFile('greeting.text',`Hello ! ${user.username}`,(err)=>{
     if(err)
     {
         console.log("unable to append the file");
