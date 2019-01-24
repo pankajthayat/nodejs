fc61f552ca9b997fb420306a7f2c487d68b2fa6b
pankajthayat <pankajthayat@gmail.com>
2019-01-24 22:33:29 +0530

    export property

--- app.js
+++ app.js
@@ -5,5 +5,14 @@ var user=os.userInfo();
 
 const notes=require('./notes');// give reletive path... this is nota module but a file... so reletive path
 
-var res=notes.addNote();
-console.log(res)
+fs.appendFile('greeting.text',`Hello ! ${user.username}. you are ${notes.age}`,(err)=>{
+    if(err)
+    {
+        console.log("unable to append the file");
+    }
+    else{
+        console.log("append successfully");
+    }
+})
+
+//most of Node's API will pass an error as the first argument to all of its functions known as error-first callbacks
