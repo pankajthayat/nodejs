f42510f8196cbe235a6fd9eda2f9b7b8a1231cc4
pankajthayat <pankajthayat@gmail.com>
2019-01-31 18:39:35 +0530

    yargs options added

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,25 @@
-const argv =require('yargs').argv;
+const yargs =require('yargs');
 const request=require('request');
 console.log("app starting..........")
+
+const request = require('request');
+const argv=yargs
+.options({
+a:{
+demand: true,
+alias: 'address',
+describe:"address to fetch weather for ",
+string:true
+}
+})
+.help()
+.alias('help','h')// alias for help
+.argv;// takes all the config
+// string : takes boolean ... this tells yargs to always parse the a
+//as string not sth else... this is just to
+// make our app full proof
+
+
 let address=argv.address;
 
 address=encodeURIComponent(address);
